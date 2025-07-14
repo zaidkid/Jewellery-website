deb6ee33b55d2ad52dd2d124a1e32d833a1b46a0
zaidkid <zaidkidwai2@gmail.com>
2025-07-14 19:56:53 +0530

    Done Bro

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // 👇 Add custom rule overrides here
+  {
+    rules: {
+      "@typescript-eslint/no-unused-expressions": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
