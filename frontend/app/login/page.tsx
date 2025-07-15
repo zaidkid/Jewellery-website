ad0b40976446f9f27325d8e36257d9888cfe1624
zaidkid <zaidkidwai2@gmail.com>
2025-07-15 21:51:17 +0530

    Final commit

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -17,7 +17,7 @@ export default function LoginPage() {
     e.preventDefault();
 
     try {
-      const res = await fetch('http://localhost:5000/api/auth/login', {
+      const res = await fetch('https://jewellery-website-7xbn.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
